69105f39237902b68dcf4c16442df8bec99611c2
Keisuke Yamauchi <keisuke.yamauchi@kechiiiiin.com>
2026-03-08 12:16:49 +0900

    update: rss更新

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,52 +1,67 @@
 import rss from '@astrojs/rss';
 import { siteConfig } from '../config';
-import { getCollection } from 'astro:content';
-import { filterPublishedEmonicles } from '../utils/posts';
+import { fetchAllBlogs, fetchAllDiaries, fetchAllEmonicles } from '../utils/content-fetcher';
+import { filterPublishedPosts, filterPublishedEmonicles } from '../utils/posts';
 import { getDiaryPath } from '../utils/date';
 
 export async function GET(context) {
-  const blogCollection = 'blog';
-  // id,slug,body,collection,data,render
-  const blog = await getCollection(blogCollection);
-  const blogItems = blog.map((post) => ({
-    title: post.data.title,
-    description: post.data.description,
-    link: context.site + blogCollection + '/' + post.data.id,
-    pubDate: post.data.date,
-    content: post.body
-  }));
-  const diaryCollection = 'diary';
-  const diary = await getCollection(diaryCollection);
-  const diaryItems = diary.map((post) => ({
+  // Blog
+  const blog = await fetchAllBlogs();
+  const publishedBlogs = filterPublishedPosts(blog);
+  const blogItems = publishedBlogs.map((post) => {
+    const content = post.source === 'microcms'
+      ? post.body // microCMSはHTMLコンテンツ
+      : post.body; // MarkdownもMarkdownコンテンツ（Astroが処理）
+
+    return {
+      title: post.data.title,
+      description: post.data.description || '',
+      link: context.site + 'blog/' + post.id,
+      pubDate: post.data.date,
+      content: content
+    };
+  });
+
+  // Diary
+  const diary = await fetchAllDiaries();
+  const publishedDiaries = filterPublishedPosts(diary);
+  const diaryItems = publishedDiaries.map((post) => {
+    const content = post.source === 'microcms'
+      ? post.body
+      : post.body;
+
+    return {
       title: post.data.title,
       description: post.data.title,
-      link: context.site + getDiaryPath(post.data.date).slice(1), // Remove leading slash
+      link: context.site + getDiaryPath(post.data.date).slice(1),
       pubDate: post.data.date,
-      content: post.body
-  }));
-  
-  const emonicleCollection = 'emonicle';
-  const emonicle = await getCollection(emonicleCollection);
+      content: content
+    };
+  });
+
+  // Emonicle
+  const emonicle = await fetchAllEmonicles();
   const publishedEmonicles = filterPublishedEmonicles(emonicle);
-  const emonicleItems = publishedEmonicles.map((post) => ({
-    title: post.data.title,
-    description: post.data.description,
-    link: context.site + emonicleCollection + '/' + post.data.id,
-    pubDate: post.data.date,
-    content: post.body
-  }));
-  
+  const emonicleItems = publishedEmonicles.map((post) => {
+    const content = post.source === 'microcms'
+      ? post.body
+      : post.body;
+
+    return {
+      title: post.data.title,
+      description: post.data.description || '',
+      link: context.site + 'emonicle/' + post.id,
+      pubDate: post.data.date,
+      content: content
+    };
+  });
+
   const allItems = [...blogItems, ...diaryItems, ...emonicleItems];
+
   return rss({
-    // `<title>` field in output xml
     title: siteConfig.title,
-    // `<description>` field in output xml
     description: siteConfig.description || siteConfig.slogan,
-    // Pull in your project "site" from the endpoint context
-    // https://docs.astro.build/en/reference/api-reference/#site
     site: context.site,
-    // Array of `<item>`s in output xml
-    // See "Generating items" section for examples using content collections and glob imports
     items: allItems,
   });
 }
